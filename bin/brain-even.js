7b2e3ab3cf45dec87ab8ba12630438bb6144e3d7
Galina Slepcova <galina20040323@gmail.com>
2025-02-28 14:44:52 +0300

    commit #9fail

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
-import { isEven }  from "../src/cli.js"
+import { isEven }  from "../src/games.js"
 isEven();
 isEven();
 isEven();
