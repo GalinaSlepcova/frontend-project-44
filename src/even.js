22763fc3a6a1b036d92f116bce57ae3809715ea2
Galina Slepcova <galina20040323@gmail.com>
2025-03-07 05:34:15 +0300

    commit fail#7

--- src/even.js
+++ src/even.js
@@ -18,7 +18,7 @@ export const isEven = () => {
     if (answer1  || answer2) {
         console.log('Correct!');
       } else {
-        const errorMessage = `"${gameUser}" is wrong answer ;(. Correct answer was ';('. /n Let's try again, ${userName}!`;
+        const errorMessage = `"${gameUser}" is wrong answer ;(. Correct answer was ';('. /n Let's try again, "${userName}"!`;
         console.log(errorMessage);
         return errorMessage;
       };
